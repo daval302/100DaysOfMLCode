1f5512f8626fc639a3fc7b808389fdb50128d105
David <daval302@gmail.com>
2018-07-04 15:57:11 +0100

    login

--- js/stmath.js
+++ js/stmath.js
@@ -6,7 +6,7 @@
 	.controller('MainController', function($scope, $rootScope, $http){
 
 		$http.defaults.headers.common.Accept = 'application/json';
-		$http.defaults.heroku = 'https://stmath-server.herokuapp.com/api/';
+		$http.defaults.heroku = 'http://stmath-server.herokuapp.com/api/';
 
 		// pay attention on data server
 		$rootScope.$on('server-response',function(event, data){
@@ -19,10 +19,23 @@
 		//$rootScope.$broadcast('server-response', [{name : "Davide", age: 30}, {name : "Francesco", age: 27}]);
 	})
 
+	.controller('LoginController', function($scope, $rootScope, $http){
+		$scope.submit = function(){
+			$rootScope.$broadcast('server-response', {email: $scope.email, password: $scope.password});
+		}
+	})
+
 	.directive('employeeList', function(){
 		return {
 			templateUrl: 'views/employee-list.html',
 			controller: 'EmployeeListController'
+		}
+	})
+
+	.directive('login', function(){
+		return {
+			templateUrl: 'views/login.html',
+			controller: 'LoginController'
 		}
 	})
 
