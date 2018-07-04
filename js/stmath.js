def313aacfea900d5d1eb07278378b67a0608fa8
David <daval302@gmail.com>
2018-07-04 14:27:29 +0100

    views setting up

--- js/stmath.js
+++ js/stmath.js
@@ -7,5 +7,18 @@
 		$scope.me = "Davide";
 	})
 
+	.controller('EmployeeListController', function($scope){
+		$scope.me = "list";
+	})
+
+	.directive('employeeList', function(){
+		return {
+			templateUrl: 'views/employee-list.html',
+			controller: 'EmployeeListController'
+		}
+	})
+
+
+
 
 })(window.angular);
