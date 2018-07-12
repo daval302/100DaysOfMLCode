1b18dd4e0d5ed853350b9904b558af7cef740e45
David <daval302@gmail.com>
2018-07-12 10:59:26 +0100

    hardcoding on data, get handy with python numpy array

--- example.py
+++ example.py
@@ -1,11 +1,24 @@
 import numpy as np
 
+def get_employees_list():
+	# gonna use hardcoding for now, later real database
+	employees_list ="RUTIGLIANO, MICCOLI, MAROTTI, MASTRODONATO, MOLITERNI, LOCORRIERE, MARCHESANO, ROTOLO, MASTROROCCO, LONGOBUCCO, MONFREDA, MORGESE, LABANI, LAVARRA, COLUCCI, CENTRONE, DELFINE, VALERIO, PUGLIESE, GIAMPAOLO, DONGHIA, ALLEGRINI, CATINIELLO, ANDRIA, ANGELINI, NOTARNICOLA, CHIEPPA, MORAMARCO, CARBONARA, DE, MONTALBO, VITALE, GUSMAI, GRECO, ZITO, PERRONE, SIFANNO, LIUZZI, ANGELINI, CORMIO, PAPPALARDO, NETTI, PALMISANO, PANARO, GIAMPALMO, LISO, GIOIA, CAMPANELLA, LIUZZI, MARCHIONNA, GIUSTINO, NINIVAGGI, DIDONNA, PALMIROTTA, LEOGRANDE, PARISI, DAMMICCO, CONVERTINO, RELLA, ZECCHINO"
+	employees = employees_list.split(" ,")
+	# associate integer numner as id for each as on DB are recrodered
+	## example return
+	return {1: "RUTIGLIANO", 2: "MICCOLI", 3: "MARIOTTI"}
+
+
 class NeuralNetwork():
 
 	def __init__(self):
 		random.seed(1)
 
 if __name__ == "__main__":
+
+	# simulate employees list getted from DB
+
+	# ...
 
 	# inputs set like id of employee from database
 	# lets say for example to use 10 employees :
@@ -16,7 +29,7 @@ if __name__ == "__main__":
 	# as starting I will tinnk to make it simple as:
 	# NeuralNetwork.train( [[0 0 0 0 0 0 1 1],[0 0 0 0 1 0 0 0]] , [[1, 0]] ) where 1 means "ON" and 0 means "OFF work" 
 
-	print ( employees_ids )
+	print ( get_employees_list() )
 
 
 
