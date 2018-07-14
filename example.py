6249a2cf54772f4b5b110abc18a5ae1801c649cb
David <daval302@gmail.com>
2018-07-14 17:39:13 +0100

    defining synapses

--- example.py
+++ example.py
@@ -1,23 +1,11 @@
 import numpy as np
 
-def get_employees_list():
-	# gonna use hardcoding for now, later real database
-	employees_list ="RUTIGLIANO, MICCOLI, MAROTTI, MASTRODONATO, MOLITERNI, LOCORRIERE, MARCHESANO, ROTOLO, MASTROROCCO, LONGOBUCCO, MONFREDA, MORGESE, LABANI, LAVARRA, COLUCCI, CENTRONE, DELFINE, VALERIO, PUGLIESE, GIAMPAOLO, DONGHIA, ALLEGRINI, CATINIELLO, ANDRIA, ANGELINI, NOTARNICOLA, CHIEPPA, MORAMARCO, CARBONARA, DE, MONTALBO, VITALE, GUSMAI, GRECO, ZITO, PERRONE, SIFANNO, LIUZZI, ANGELINI, CORMIO, PAPPALARDO, NETTI, PALMISANO, PANARO, GIAMPALMO, LISO, GIOIA, CAMPANELLA, LIUZZI, MARCHIONNA, GIUSTINO, NINIVAGGI, DIDONNA, PALMIROTTA, LEOGRANDE, PARISI, DAMMICCO, CONVERTINO, RELLA, ZECCHINO"
-	employees = employees_list.split(", ")
-	ritorno = dict(  zip( range( len(employees) ), employees ) )
-	# associate integer numner as id for each as on DB are recrodered
-	# but I need to produce and input like : [[0 0 0 0 0 0 1 1], [0,0,1,0,0,0,0] ]
-	## the second argument input represent the day of the week : 1 on the week position array
-
-	return  ritorno
-
-
 class NeuralNetwork():
 
 	def __init__(self):
 		np.random.seed(1)
 		# data to be query from DB
-		self.employees = get_employees_list()
+		self.employees = self.getEmployees()
 		# input variables
 		self.alpha = 0.1
 		self.input_dim = 2
@@ -31,6 +19,22 @@ class NeuralNetwork():
 		syn_0_update = np.zeros_like(syn_0)
 		syn_1_update = np.zeros_like(syn_1)
 		syn_h_update = np.zeros_like(syn_h)
+
+	def getEmployees(self):
+		# gonna use hardcoding for now, later real database
+		employees_list ="RUTIGLIANO, MICCOLI, MAROTTI, MASTRODONATO, MOLITERNI, LOCORRIERE, MARCHESANO, ROTOLO, MASTROROCCO, LONGOBUCCO, MONFREDA, MORGESE, LABANI, LAVARRA, COLUCCI, CENTRONE, DELFINE, VALERIO, PUGLIESE, GIAMPAOLO, DONGHIA, ALLEGRINI, CATINIELLO, ANDRIA, ANGELINI, NOTARNICOLA, CHIEPPA, MORAMARCO, CARBONARA, DE, MONTALBO, VITALE, GUSMAI, GRECO, ZITO, PERRONE, SIFANNO, LIUZZI, ANGELINI, CORMIO, PAPPALARDO, NETTI, PALMISANO, PANARO, GIAMPALMO, LISO, GIOIA, CAMPANELLA, LIUZZI, MARCHIONNA, GIUSTINO, NINIVAGGI, DIDONNA, PALMIROTTA, LEOGRANDE, PARISI, DAMMICCO, CONVERTINO, RELLA, ZECCHINO"
+		employees = employees_list.split(", ")
+		ritorno = dict(  zip( range( len(employees) ), employees ) )
+		# associate integer numner as id for each as on DB are recrodered
+		# but I need to produce and input like : [[0 0 0 0 0 0 1 1], [0,0,1,0,0,0,0] ]
+		## the second argument input represent the day of the week : 1 on the week position array
+
+		return ritorno
+
+	#def getSynapses(self):
+		# synapses are will be stored into DB
+		# this is hardcoding
+
 
 
 if __name__ == "__main__":
@@ -55,6 +59,7 @@ if __name__ == "__main__":
 	"""
 
 	n = NeuralNetwork()
+	print(n.employees)
 
 
 
