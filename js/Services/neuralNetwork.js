c2a261de396559439b7f6fdbeb96dfee85912cf0
David <daval302@gmail.com>
2018-09-19 13:03:29 +0100

    Initialized synapses and updated input ID siize based on json sample

--- js/Services/neuralNetwork.js
+++ js/Services/neuralNetwork.js
@@ -10,7 +10,7 @@
 	Output : [shift pattern X 7 ] (168 bit)
 
 	Note: [ID] from DB by default is a 4 byte size, so to reduce computational cost, I will rapresent in 
-	Angular service as a 8 bit vlaue, so a range of unisgned integer of [0,255] that will be enought for
+	Angular service as a 16 bit vlaue, so a range of unisgned integer between [0, 65535] that will be enought for
 	not production environment
 
 */
@@ -26,11 +26,22 @@ angular.module('stmath')
 		return x * (1 - x);
 	}
 
-	// input variables
+	// Input variables
 	var alpha = 0.1
 	var input_dim = 2
-	var hidden_dim = 176
+	var hidden_dim = 2688
 	var output_dim = 1
+
+	// Initialize neural network weights
+	var synapse_0 = math.multiply( 2, math.add( math.random([input_dim, hidden_dim]), -1) ) ;
+	var synapse_1 = math.multiply( 2, math.add( math.random([hidden_dim, output_dim]), -1) ) ;
+	var synapse_h = math.multiply( 2, math.add( math.random([hidden_dim, hidden_dim]), -1) ) ;
+
+	var synapse_0_update = math.zeros( math.size(synapse_0) );
+	var synapse_1_update = math.zeros( math.size(synapse_1) );
+	var synapse_h_update = math.zeros( math.size(synapse_h) );
+
+	// Training logic
 
 	return {
 		sigmoid
